44eb611ceae7115bcba36f0955a964267d482cf7
yafuu822 <yafuu822@yahoo.co.jp>
2018-07-04 22:27:42 +0900

    記事一覧で表示するコンテンツに文字数制限をかける形で修正

--- blog/template-parts/content.php
+++ blog/template-parts/content.php
@@ -18,22 +18,6 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-        <?php echo the_excerpt(); ?>
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-				get_the_title()
-			) );
-
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
-				'separator'   => '<span class="screen-reader-text">, </span>',
-			) );
-		?>
+		<?php echo the_excerpt(); ?>
 	</div><!-- .entry-content -->
 </article><!-- #post-## -->
