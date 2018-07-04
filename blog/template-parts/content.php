600f5ef9b644b9ad16a2418d23c2a53bfe02c095
yafuu822 <yafuu822@yahoo.co.jp>
2018-07-04 22:14:05 +0900

    記事一覧トップの関数を削除

--- blog/template-parts/content.php
+++ blog/template-parts/content.php
@@ -13,15 +13,12 @@
 		<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
 			<span class="sticky-post"><?php _e( 'Featured', 'twentysixteen' ); ?></span>
 		<?php endif; ?>
-
+        <?php echo get_the_date();  ?>
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php twentysixteen_excerpt(); ?>
-
-	<?php twentysixteen_post_thumbnail(); ?>
-
 	<div class="entry-content">
+        <?php echo the_excerpt(); ?>
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
@@ -39,19 +36,4 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php twentysixteen_entry_meta(); ?>
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
